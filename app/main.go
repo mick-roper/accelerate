a819192c61199df7af7ea86ffd8c2212e52bf512
mick-roper <mick@theropers.co.uk>
2020-01-09 14:53:31 +0000

    added calculations

--- app/main.go
+++ app/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"log"
 	"fmt"
+	"log"
 )
 
 type iLogger interface {
@@ -15,8 +15,10 @@ type iLogger interface {
 var targetDistance = flag.Float64("distance", -1, "the distance you want to travel")
 var acceleration = flag.Float64("acceleration", -1, "the amount of linear acceleration")
 var logLevel = flag.String("log-level", "info", "sets the log level")
+var withDeceleration = flag.Bool("with-deceleration", false, "(true) to include deceleration in the plan")
 
 var logger iLogger
+var calc func(float64, float64, float64, float64) (float64, float64)
 
 func main() {
 	flag.Parse()
@@ -35,6 +37,12 @@ func main() {
 		log.Fatal("acceleration is invalid")
 	}
 
+	if *withDeceleration {
+		calc = calculateTransitWithDeceleration
+	} else {
+		calc = calculateTransit
+	}
+
 	var seconds float64 = 0
 	var travelled float64 = 0
 	var speed float64 = 0
@@ -43,18 +51,43 @@ func main() {
 
 	for travelled < *targetDistance {
 		seconds++
-		speed += *acceleration
-		travelled += speed
+		speed, travelled = calc(speed, travelled, *acceleration, *targetDistance)
 
-		logger.Debug(fmt.Sprintf("%vm @ %vms", travelled, speed))
+		logger.Debug(fmt.Sprintf("%fm @ %fms", travelled, speed))
 	}
 
-	log.Printf("target reached in %f seconds OR %f minutes OR %f hours", seconds, seconds / 60, seconds /60 / 60)
+	log.Printf("target reached in %f seconds OR %f minutes OR %f hours", seconds, seconds/60, seconds/60/60)
 }
 
-type debugLogger struct {}
+func calculateTransit(speed, travelled, acceleration, targetDistance float64) (float64, float64) {
+	newSpeed := speed + acceleration
+	return newSpeed, travelled + newSpeed
+}
 
-type infoLogger struct {}
+func calculateTransitWithDeceleration(speed, travelled, acceleration, targetDistance float64) (float64, float64) {
+	var newSpeed float64
+	var newAcc float64
+	
+	if acceleration > targetDistance - travelled {
+		newAcc = (targetDistance - travelled) / 2
+	} else {
+		newAcc = acceleration
+	}
+
+	if travelled >= targetDistance*0.50 {
+		newAcc = newAcc * -1
+	}
+
+	newSpeed = speed + newAcc
+
+	if newSpeed < 0 {
+		newSpeed = 1
+	}
+
+	return newSpeed, travelled + newSpeed
+}
+
+type debugLogger struct{}
 
 func (l *debugLogger) Debug(x string) {
 	log.Print(x)
@@ -67,6 +100,8 @@ func (l *debugLogger) Info(x string) {
 func (l *debugLogger) Fatal(x string) {
 	log.Fatal(x)
 }
+
+type infoLogger struct{}
 
 func (l *infoLogger) Debug(x string) {}
 
